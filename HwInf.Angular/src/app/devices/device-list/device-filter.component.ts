a0817d6f37e2a2703938013760d3a4928741b1e2
Jan Calanog <nejcalanog@gmail.com>
2016-12-06 23:10:34 +0100

    derp

--- HwInf.Angular/src/app/devices/device-list/device-filter.component.ts
+++ HwInf.Angular/src/app/devices/device-list/device-filter.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit, OnDestroy} from '@angular/core';
 import {DeviceService} from "../device.service";
 import {ActivatedRoute} from "@angular/router";
-import {Subscription} from "rxjs";
+import {Subscription, Observable} from "rxjs";
 import {IDictionary} from "../../shared/IDictionary";
 import {Dictionary} from "../../shared/Dictionary";
 import {Response} from "@angular/http";
@@ -15,7 +15,6 @@ export class DeviceFilterComponent implements OnInit, OnDestroy {
     private subscription: Subscription;
     private components: string[] = [];
     private currentType: string;
-    private dict: IDictionary<string[]> = new Dictionary<string[]>();
 
     constructor(private deviceService: DeviceService, private route: ActivatedRoute) {}
 
@@ -27,20 +26,14 @@ export class DeviceFilterComponent implements OnInit, OnDestroy {
                     this.deviceService.getComponents(this.currentType).subscribe(
                         (data: string[]) => {
                             this.components = data;
-
-                            for (let component of this.components) {
-                                this.getComponentValues(this.currentType,component);
-                            }
                         }
                     );
                 }
             );
-
     }
 
     getComponentValues(type: string, component: string) {
-
-      console.log(this.deviceService.getComponentValues(type, component));
+        return this.deviceService.getComponentValues(type, component);
     }
 
     ngOnDestroy() {
--- HwInf.Angular/src/app/devices/device.service.ts
+++ HwInf.Angular/src/app/devices/device.service.ts
@@ -3,6 +3,7 @@ import {Device} from "./device";
 import {Http, Response} from "@angular/http";
 import 'rxjs/Rx';
 import {Dictionary} from "../shared/Dictionary";
+import {Observable} from "rxjs";
 
 @Injectable()
 export class DeviceService {
@@ -10,28 +11,28 @@ export class DeviceService {
 
     constructor(private http: Http) {}
 
-    getDevices(type: string = "") {
+    getDevices(type: string = ""): Observable<Device[]> {
         return this.http.get('http://localhost:14373/api/devices/' + type)
             .map((response: Response) => response.json());
     }
 
-    getDevice(id: number) {
+    getDevice(id: number): Observable<Device[]> {
         return this.http.get('http://localhost:14373/api/devices/id/' + id)
             .map((response: Response) => response.json());
     }
 
-    getTypes() {
+    getTypes(): Observable<string[]> {
         return this.http.get('http://localhost:14373/api/devices/types/')
             .map((response: Response) => response.json());
     }
 
-    getComponents(type: string) {
+    getComponents(type: string): Observable<string[]> {
         return this.http.get('http://localhost:14373/api/devices/components/' + type)
             .map((response: Response) => response.json());
     }
 
-    getComponentValues(type: string, component: string) {
-        return this.http.get('http://localhost:14373/api/devices/components/' + type + '/' + component)
+    getComponentValues(type: string, component: string): Observable<string[]> {
+        return this.http.get('http://localhost:14373/api/devices/components/' + type + '/' + component.toLowerCase())
             .map((response: Response) => response.json());
     }
 }
