44f338b907a32425d7b91338a96121719dfa2ffe
Tobias Schlachter <tobias.schlachter@technikum-wien.at>
2016-12-18 19:01:15 +0100

    Seed edited. DELETE DATABASE BEFORE RUNNING

--- HwInf.Common/Migrations/Configuration.cs
+++ HwInf.Common/Migrations/Configuration.cs
@@ -17,7 +17,7 @@ namespace HwInf.Common.Migrations
 
         protected override void Seed(HwInf.Common.DAL.HwInfContext context)
         {
-            /**
+           
             var count = context.Devices.Count();
 
             if(count > 0)
@@ -44,14 +44,46 @@ namespace HwInf.Common.Migrations
                 new Status { Description = "In Reparatur" }
             };
 
+            status.ForEach(s => context.Status.Add(s));
+
+            var rooms = new List<Room>
+            {
+                new Room { Name = "A0.00" },
+                new Room { Name = "F0.00" },
+                new Room { Name = "B0.00" }
+            };
+
+            rooms.ForEach(s => context.Rooms.Add(s));
+
+            var roles = new List<Role>
+            {
+                new Role { Name = "Admin" },
+                new Role { Name = "User" },
+                new Role { Name = "Owner" }
+            };
+
+            roles.ForEach(s => context.Roles.Add(s));
+
+            var persons = new List<Person>
+            {
+                new Person { Name = "Jan", LastName = "Calanog", Email = "jan.calanog.technikum-wien.at", Role = roles.Single(i => i.Name == "Admin"), uid = "if15b042" },
+                new Person { Name = "Tobias", LastName = "Schlachter", Email = "tobias.schlachter@technikum-wien.at", Role = roles.Single(i => i.Name == "Admin"), uid = "if15b032" },
+                new Person { Name = "Valentin", LastName = "Sagl", Email = "valentin.sagl@technikum-wien.at", Role = roles.Single(i => i.Name == "Admin"), uid = "if15b030" },
+                new Person { Name = "Sebastian", LastName = "Slowak", Email = "sebastian.slowak@technikum-wien.at", Role = roles.Single(i => i.Name == "Admin"), uid = "if15b049" }
+            };
+
+
+            persons.ForEach(s => context.Persons.Add(s));
+
+
             var dev = new List<Device>
            {
-            new Device { Description = "Lenovo Notebook", Brand = "Lenovo", Status = status.Single(i => i.Description == "Verfügbar"), InvNum = "a123", Type = type.Single(i => i.Description == "Notebook")},
-            new Device { Description = "Acer PC", Brand = "Acer", Status = status.Single(i => i.Description == "Ausgeliehen"), InvNum = "a5123", Type = type.Single(i => i.Description == "PC")},
-            new Device { Description = "Benq Monitor", Brand = "Benq", Status = status.Single(i => i.Description == "Verfügbar"), InvNum = "a6123", Type = type.Single(i => i.Description == "Monitor")},
-            new Device { Description = "Medion PC", Brand = "Medion", Status = status.Single(i => i.Description == "In Reparatur"), InvNum = "a57123", Type = type.Single(i => i.Description == "PC")},
-            new Device { Description = "HP PC", Brand = "HP", Status = status.Single(i => i.Description == "Verfügbar"), InvNum = "a985123", Type = type.Single(i => i.Description == "PC")},
-            new Device { Description = "Acer PC", Brand = "Acer", Status = status.Single(i => i.Description == "Ausgeliehen"), InvNum = "a512683", Type = type.Single(i => i.Description == "PC")}
+            new Device { Name = "Lenovo Notebook", Brand = "Lenovo", Status = status.Single(i => i.Description == "Verfügbar"), InvNum = "a123", Type = type.Single(i => i.Description == "Notebook"), CreateDate = DateTime.Now, Room = rooms.Single(i => i.Name == "A0.00"), Person = persons.Single(i => i.LastName == "Calanog")},
+            new Device { Name = "Acer PC", Brand = "Acer", Status = status.Single(i => i.Description == "Ausgeliehen"), InvNum = "a5123", Type = type.Single(i => i.Description == "PC"), CreateDate = DateTime.Now, Room = rooms.Single(i => i.Name == "A0.00"), Person = persons.Single(i => i.LastName == "Calanog")},
+            new Device { Name = "Benq Monitor", Brand = "Benq", Status = status.Single(i => i.Description == "Verfügbar"), InvNum = "a6123", Type = type.Single(i => i.Description == "Monitor"), CreateDate = DateTime.Now, Room = rooms.Single(i => i.Name == "F0.00"), Person = persons.Single(i => i.LastName == "Calanog")},
+            new Device { Name = "Medion PC", Brand = "Medion", Status = status.Single(i => i.Description == "In Reparatur"), InvNum = "a57123", Type = type.Single(i => i.Description == "PC"), CreateDate = DateTime.Now, Room = rooms.Single(i => i.Name == "F0.00"), Person = persons.Single(i => i.LastName == "Calanog")},
+            new Device { Name = "HP PC", Brand = "HP", Status = status.Single(i => i.Description == "Verfügbar"), InvNum = "a985123", Type = type.Single(i => i.Description == "PC"), CreateDate = DateTime.Now, Room = rooms.Single(i => i.Name == "B0.00"), Person = persons.Single(i => i.LastName == "Sagl")},
+            new Device { Name = "Acer PC", Brand = "Acer", Status = status.Single(i => i.Description == "Ausgeliehen"), InvNum = "a512683", Type = type.Single(i => i.Description == "PC"), CreateDate = DateTime.Now, Room = rooms.Single(i => i.Name == "B0.00"), Person = persons.Single(i => i.LastName == "Sagl")}
            };
 
             dev.ForEach(s => context.Devices.Add(s));
@@ -78,7 +110,7 @@ namespace HwInf.Common.Migrations
 
             devMeta.ForEach(s => context.DeviceMeta.Add(s));
             context.SaveChanges();
-    **/
+
             base.Seed(context);
         }
     }
