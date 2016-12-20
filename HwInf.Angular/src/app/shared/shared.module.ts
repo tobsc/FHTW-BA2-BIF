056838968c06175da610c7a3265ce99be1a58094
Jan Calanog <nejcalanog@gmail.com>
2016-12-20 23:37:50 +0100

    Added: Bootstrap 3 dropdown directive

--- HwInf.Angular/src/app/shared/shared.module.ts
+++ HwInf.Angular/src/app/shared/shared.module.ts
@@ -1,18 +1,20 @@
 import {NgModule} from "@angular/core";
 import {ModalComponent} from "./modal/modal.component";
 import {CommonModule} from "@angular/common";
-import {PanelCollapseDirective} from "./panel-collapse.directive";
+import {PanelCollapseDirective} from "./bootstrap/panel-collapse.directive";
 import {SortPipe} from "./pipes/sort.pipe";
 import {NavComponent} from "../nav.component";
 import { ErrorMessageComponent } from './error-message/error-message.component';
 import {RouterModule} from "@angular/router";
+import { DropdownDirective } from './bootstrap/dropdown.directive';
 @NgModule({
   declarations: [
     ModalComponent,
     PanelCollapseDirective,
     SortPipe,
     NavComponent,
-    ErrorMessageComponent
+    ErrorMessageComponent,
+    DropdownDirective
   ],
   imports: [
     CommonModule,
