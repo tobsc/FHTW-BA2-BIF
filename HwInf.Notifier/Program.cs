237ee2c69438cb164401fb773f7f584fcbe009a2
Valentin Sagl <if15b030@technikum-wien.at>
2017-01-23 18:08:04 +0100

    Notifier init

---
+++ HwInf.Notifier/Program.cs
@@ -0,0 +1,56 @@
+﻿using HwInf.Common;
+using HwInf.Common.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("We now create the Notifier");
+            Notifier notify = new Notifier(DateTime.Now.Date);
+            
+        }
+    }
+
+    public class Notifier
+    {
+        
+        public Notifier(DateTime date)
+        {
+            HwInfContext db = new HwInfContext();
+            Console.WriteLine("We now are looking into the DB");
+            var uidlist = db.Orders.Where(i => i.ReturnDate.Date == date.Date).Select(i => i.Owner.uid);
+            Console.WriteLine("We now create the Mail");
+            if (uidlist.Count() > 0)
+            {
+                foreach (var uid in uidlist)
+                {
+                    var mailadress = db.Persons.Where(i => i.uid == uid).Select(i => i.Email).SingleOrDefault();
+                    var act = @"Liebe/r StudentIn, 
+                    Sie haben bei uns ein oder mehrere Geräte ausgeborgt, und heute ist der vereinbarte Termin für die Rückgabe.
+                    Bitte vergessen Sie es nicht!
+                    Mit freundlichen Grüßen,
+                    Ihr HW-Inf Team
+                
+                    PS: Dies ist eine automatische Mail. Bitte nicht darauf Antowrten";
+
+                    Mail mail = new Mail();
+                    mail.To(mailadress);
+                    mail.Message(act);
+                    mail.Send();
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("No Orders expiring today");
+            }
+        }
+    }
+}
