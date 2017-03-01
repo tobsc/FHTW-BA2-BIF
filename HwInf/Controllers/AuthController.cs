a72d6ce22d2d7703d608d4ef1376621d9a427833
Tobias Schlachter <if15b032@technikum-wien.at>
2017-03-01 15:16:10 +0100

    another auth fix

--- HwInf/Controllers/AuthController.cs
+++ HwInf/Controllers/AuthController.cs
@@ -15,7 +15,7 @@ using System.Web.Http.Description;
 
 namespace HwInf.Controllers
 {
-    [RoutePrefix("api/autha")]
+    [RoutePrefix("api/auth")]
     public class AuthControllerA : ApiController
     {
 
@@ -30,7 +30,34 @@ namespace HwInf.Controllers
             
             if (ModelState.IsValid)
             {
-                if (vmdl.Uid.Equals("admin"))
+                //if (LDAPAuthenticator.Authenticate(vmdl.Uid, vmdl.Password).IsAuthenticated)
+                //{
+                //    Person p;
+
+                //    if (db.Persons.Where(i => i.uid == vmdl.Uid).Count() > 0)
+                //    {
+                //        p = db.Persons.Single(i => i.uid == vmdl.Uid);
+                //    }
+                //    else
+                //    {
+
+                //        p = new Person();
+                //        var ldapUser = LDAPAuthenticator.Authenticate(vmdl.Uid, vmdl.Password);
+                //        vmdl.Refresh(ldapUser);
+                //        vmdl.ApplyChanges(p, db);
+                //        db.Persons.Add(p);
+                //        db.SaveChanges();
+                //    }
+
+                //    vmdl.Refresh(p);
+                //    var token = CreateToken(p);
+
+                //    return Ok(new { token });
+                //}
+                //else
+
+
+               if (vmdl.Uid.Equals("admin"))
                 {
                     Person p = new Person { uid = vmdl.Uid, Name = "Admin", Role = db.Roles.Single(i => i.Name == "Admin") };
                     var token = CreateToken(p);
