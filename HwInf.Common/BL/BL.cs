e808018d5f7bf44c577f5b7ac74a137a453ba6c8
Tobias Schlachter <if15b032@technikum-wien.at>
2017-03-03 18:04:22 +0100

    BL added

---
+++ HwInf.Common/BL/BL.cs
@@ -0,0 +1,31 @@
+﻿using HwInf.Common.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace HwInf.Common.BL
+{
+    public class BL
+    {
+        private HwInfContext _dal;
+
+        public BL() {
+            _dal = new HwInfContext();
+        }
+        public BL(HwInfContext dal)
+        {
+            _dal = dal;
+        }
+
+        public IQueryable<Device> GetDevices(bool onlyActive = true)
+        {
+
+            return _dal.Devices.Include(x => x.Type)
+                .Where(i => i.DeviceId > 0) // TODO: where is active
+                .Take(10000);
+        }
+    }
+}
