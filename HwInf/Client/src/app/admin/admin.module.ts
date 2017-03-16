18ae5e663237c563439736af215ca2740e90bd59
Jan Calanog <nejcalanog@gmail.com>
2017-03-16 22:45:59 +0100

    DevicesTypes Templates erstellt

--- HwInf/Client/src/app/admin/admin.module.ts
+++ HwInf/Client/src/app/admin/admin.module.ts
@@ -6,11 +6,17 @@ import {adminRouting} from "./admin.routing";
 import { DeviceListComponent } from './devices/device-list/device-list.component';
 import {CoreModule} from "../core/core.module";
 import { DeviceAddComponent } from './devices/device-add/device-add.component';
+import { DeviceTypesComponent } from './devices/device-types/device-types.component';
+import { DeviceTypesListComponent } from './devices/device-types/device-types-list/device-types-list.component';
+import { DeviceTypesAddComponent } from './devices/device-types/device-types-add/device-types-add.component';
 @NgModule({
     declarations: [
         AdminDashboardComponent,
         DeviceListComponent,
-        DeviceAddComponent
+        DeviceAddComponent,
+        DeviceTypesComponent,
+        DeviceTypesListComponent,
+        DeviceTypesAddComponent,
     ],
     imports: [
         CoreModule,
