6c359263fd107a2cde79d82494e0b9391510aef1
Valentin Sagl <if15b030@technikum-wien.at>
2017-03-16 17:28:18 +0100

    removed device-filter

--- HwInf/Client/src/app/user/user.module.ts
+++ HwInf/Client/src/app/user/user.module.ts
@@ -3,7 +3,6 @@ import {CommonModule} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DeviceListComponent } from './devices/device-list.component';
-import { DeviceFilterComponent } from './devices/device-filter.component';
 import { DevicesStatusDirective } from './devices/devices-status.directive';
 import { CoreModule } from "../core/core.module";
 import { AccordionModule } from "ng2-bootstrap";
@@ -13,7 +12,6 @@ import { AccordionModule } from "ng2-bootstrap";
     declarations: [
         DashboardComponent,
         DeviceListComponent,
-        DeviceFilterComponent,
         DevicesStatusDirective],
     imports: [
         CommonModule,
