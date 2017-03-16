eaec84973e910f922613809a31c519eb71c120f5
Valentin Sagl <if15b030@technikum-wien.at>
2017-03-16 17:01:56 +0100

    added field and fieldgroup component

---
+++ HwInf/Client/src/app/shared/models/field.model.ts
@@ -0,0 +1,12 @@
+﻿import { DeviceType } from "./device-type.model";
+import { User } from "./user.model";
+import { Status } from "./status.model";
+import { DeviceMeta } from "./device-meta.model";
+export class Field {
+    constructor(
+        public FieldId: number,
+        public Name: string,
+        public Label: string,
+        public FieldType: string
+    ) { }
+}
---
+++ HwInf/Client/src/app/shared/models/fieldgroup.model.ts
@@ -0,0 +1,14 @@
+﻿import { DeviceType } from "./device-type.model";
+import { User } from "./user.model";
+import { Status } from "./status.model";
+import { DeviceMeta } from "./device-meta.model";
+import { Field } from "./field.model";
+
+export class Group {
+    constructor(
+        public GroupId: number,
+        public Name: string,
+        public Label: string,
+        public Field: Field[]
+    ) { }
+}
