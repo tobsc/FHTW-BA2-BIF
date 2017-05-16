745218f7947f67f9c0b9ab8298da7ddb6b694997
Tobias Schlachter <if15b032@technikum-wien.at>
2017-05-16 15:35:26 +0200

    Backend: dummy data removed

--- HwInf.Common/Migrations/Configuration.cs
+++ HwInf.Common/Migrations/Configuration.cs
@@ -19,65 +19,12 @@ namespace HwInf.Common.Migrations
         protected override void Seed(HwInf.Common.DAL.HwInfContext context)
         {
 
-            var type = new List<DeviceType>
-            {
-                new DeviceType { Name = "Notebook", Slug = "notebook", IsActive = true},
-                new DeviceType { Name = "PC", Slug = "pc", IsActive = true },
-                new DeviceType { Name = "Monitor", Slug = "monitor", IsActive = true},
-                new DeviceType { Name = "Festplatte", Slug = "festplatte", IsActive = true},
-            };
-
-            var anschluessefields = new List<Field>
-            {
-                new Field {Slug = "hdmi", Name = "HDMI"},
-                new Field {Slug = "vga", Name = "VGA"},
-            };
-
-            var prozessorfields = new List<Field>
-            {
-                new Field {Slug = "intel-i7", Name = "Intel i7"},
-                new Field {Slug = "intel-i5", Name = "Intel i5"},
-                new Field {Slug = "amd-ryzen", Name = "AMD Ryzen"},
-                new Field {Slug = "amd-athlon", Name = "AMD Athlon"},
-            };
-
-            var monitorfields = new List<Field>
-            {
-                new Field {Slug = "13-zoll", Name = "13 Zoll"},
-                new Field {Slug = "15-zoll", Name = "15 Zoll"},
-                new Field {Slug = "17-zoll", Name = "17 Zoll"},
-            };
-
-            var aufloesungfields = new List<Field>
-            {
-                new Field {Slug = "1366x768", Name = "1366x768"},
-                new Field {Slug = "1920x1080", Name = "1920x1080"},
-                new Field {Slug = "1280x1024", Name = "1280x1024"},
-            };
-
-            var festplattefields = new List<Field>
-            {
-                new Field {Slug = "128-gb", Name = "128 GB"},
-                new Field {Slug = "256-gb", Name = "256 GB"},
-                new Field {Slug = "2-tb", Name = "2 TB"},
-            };
-
-            var fieldGroup = new List<FieldGroup>
-            {
-                new FieldGroup {Slug = "anschluesse", Name = "Anschlüsse", Fields = anschluessefields.ToList(), DeviceTypes = type.Where(i => i.Slug == "pc" || i.Slug == "notebook" || i.Slug == "monitor").ToList(), IsActive = true, IsCountable = true},
-                new FieldGroup {Slug = "prozessoren", Name = "Prozessoren", Fields = prozessorfields.ToList(), DeviceTypes = type.Where(i => i.Slug == "pc" || i.Slug == "notebook").ToList(), IsActive = true, IsCountable = true},
-                new FieldGroup {Slug = "bilschirmdiagonale", Name = "Bildschirmdiagonale", Fields = monitorfields.ToList(), DeviceTypes = type.Where(i => i.Slug == "monitor" || i.Slug == "notebook").ToList(), IsActive = true, IsCountable = true},
-                new FieldGroup {Slug = "aufloesung", Name = "Auflösung", Fields = aufloesungfields.ToList(), DeviceTypes = type.Where(i => i.Slug == "monitor" || i.Slug == "notebook").ToList(), IsActive = true, IsCountable = true},
-                new FieldGroup {Slug = "kapazitaet", Name = "Kapazität", Fields = festplattefields.ToList(), DeviceTypes = type.Where(i => i.Slug == "festplatte").ToList(), IsActive = true, IsCountable = true},
-            };
-
-
-
             var deviceStatus = new List<DeviceStatus>
                 {
                     new DeviceStatus { Description = "Verfügbar" },
                     new DeviceStatus { Description = "Ausgeliehen" },
                     new DeviceStatus { Description = "In Reparatur" },
+                    new DeviceStatus { Description = "Archiviert" },
                 };
 
             var orderStatus = new List<OrderStatus>
@@ -104,31 +51,6 @@ namespace HwInf.Common.Migrations
                     new Person { Name = "Sebastian", LastName = "Slowak", Email = "sebastian.slowak@technikum-wien.at", Role = roles.Single(i => i.Name == "Admin"), Uid = "if15b049" },
             };
 
-            var meta = new List<DeviceMeta>
-            {
-                new DeviceMeta
-                {
-                    MetaValue = "2",
-                    FieldName = fieldGroup.Select(i => i.Fields.Single(x => x.Slug == "hdmi").Name).FirstOrDefault(),
-                    FieldSlug = fieldGroup.Select(i => i.Fields.Single(x => x.Slug == "hdmi").Slug).FirstOrDefault(),
-                    FieldGroupName = fieldGroup.Single(i => i.Slug == "anschluesse").Name,
-                    FieldGroupSlug = fieldGroup.Single(i => i.Slug == "anschluesse").Slug
-                },
-                                new DeviceMeta
-                {
-                    MetaValue = "5",
-                    FieldName = fieldGroup.Select(i => i.Fields.Single(x => x.Slug == "vga").Name).FirstOrDefault(),
-                    FieldSlug = fieldGroup.Select(i => i.Fields.Single(x => x.Slug == "vga").Slug).FirstOrDefault(),
-                    FieldGroupName = fieldGroup.Single(i => i.Slug == "anschluesse").Name,
-                    FieldGroupSlug = fieldGroup.Single(i => i.Slug == "anschluesse").Slug
-                }
-            };
-
-            var dev = new List<Device>
-               {
-                new Device { Name = "Acer PC", Brand = "Acer", Status = deviceStatus.Single(i => i.Description == "Verfügbar"), InvNum = "a5123", Type = type.Single(i => i.Slug == "pc"), CreateDate = DateTime.Now, Room = "A0.00", Person = persons.Single(i => i.LastName == "Calanog"), IsActive = true, DeviceMeta = meta.ToList()},
-               };
-
             var settings = new List<Setting>
             {
                new Setting { Key = "ss_start", Value = "15.02"},
@@ -151,20 +73,11 @@ namespace HwInf.Common.Migrations
                 new DamageStatus { Name = "Behoben", Slug = "behoben"}
             };
 
-            var damages = new List<Damage>
-            {
-               new Damage { Date=DateTime.Now, Cause= persons.Single(i => i.LastName == "Sagl"), Reporter= persons.Single(i=> i.LastName=="Sagl"),  Description="Display oben rechts eingebrochen", Device=dev.Single(i=>i.InvNum=="a5123"), DamageStatus=damageStatus.Single(i => i.Slug=="gemeldet")},
-            };
-
             if (!context.Settings.Any())
             {
                 context.Settings.AddRange(settings);
             }
 
-            if (!context.DeviceTypes.Any())
-            {
-                context.DeviceTypes.AddRange(type);
-            }
 
             if (!context.DeviceStatus.Any())
             {
@@ -186,27 +99,10 @@ namespace HwInf.Common.Migrations
                 context.Persons.AddRange(persons);
             }
 
-            if (!context.FieldGroups.Any())
-            {
-                context.FieldGroups.AddRange(fieldGroup);
-            }
-
-            if (!context.Devices.Any())
-            {
-                context.Devices.AddRange(dev);
-            }
-
             if (!context.DamageStatus.Any())
             {
                 context.DamageStatus.AddRange(damageStatus);
             }
-
-            if (!context.Damages.Any())
-            {
-                context.Damages.AddRange(damages);
-            }
-
-
 
             base.Seed(context);
         }
